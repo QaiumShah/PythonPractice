b6737b21abab8dac964c1572decaa99d70a7f3be
Q8Sa <qaiumshah92@gmail.com>
2019-10-11 21:15:49 +0300

    Details added as comment for each line of the code for function, class and function inside function added

--- demo.py
+++ demo.py
@@ -1,20 +1,46 @@
-def fabonacci(n):
-    a,b= 0,1
-    while a<n:
-        print(a, end=" ")
-        a,b = b, a+b
-    print()
+#function for fabonacci series desplay
+def fabonacci(n):   #passing total number 'n' in the function. 
+                    #This will be the highest number till the fabonacci series will be displayed
+    a,b= 0,1   #declaring two variables a and b with values 0 and 1 respectively
+    while a<n:      # using while loop to stop the loop till it reaches less then n starting from a
+        print(a, end=" ")       # print a with space on the same line
+        a,b = b, a+b            # a will become b and b will become a+b
+    print()                     #print new line
 
-fabonacci(2000)
+fabonacci(2000)     #initialising the function with 2000 as its n parameter .
 
+##output: 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 ##
+
+
+#calling a class object
 class Fruit:
-    def __init__(self, name, color, flavor):
-        self.name = name
-        self.color = color
-        self.flavor = flavor
+    def __init__(self, name, color, flavor):        #defining a construct method with instance 'self' and 
+                                                    #parameter of the class
+
+        self.name = name               #instance variable name assigned to name parameter
+        self.color = color             # instance variable color assigned to color parameter
+        self.flavor = flavor           # instance variable flavor assigned to flavor parameter
     
-    def description(self):
+    def description(self):          # defining a function description with instance self as parameter
+
+        #method of displaying instance variables using %s ordered inside %(self.name, ...)
         print("I'm a %s %s and I taste %s." %(self.color, self.name, self.flavor))
 
-lemon = Fruit("lemon", "yellow", "sour")
-lemon.description() 
+lemon = Fruit("lemon", "yellow", "sour")    #declaring instance of class Fruit with values
+lemon.description()         #calling the description method inside the class
+
+##output: I'm a yellow lemon and I taste sour. ##
+
+#calling function inside function
+#number represents the order of execution of the code
+def f():
+    def g():            
+        print("Hi, it's me 'g'.") #4
+        print("Thanks for calling me.") #5
+    
+    print("This is funciton 'f'.")  #1
+    print("I am calling 'g' now: ") #2
+    g() #3
+
+f()
+
