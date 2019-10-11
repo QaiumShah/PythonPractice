6647c39f3354ca192739f2920c9ca1bae0e52d93
Q8Sa <qaiumshah92@gmail.com>
2019-10-11 23:09:30 +0300

    def f deleted..  pasted by mistake

--- decorators.py
+++ decorators.py
@@ -1,7 +1,6 @@
 ## Defining a function to decorate##
 ## number represents the order of execution of the code
 
-def f():
 def decoration_func(func):   #3  #declaring function with function as parameter
     def wrapper(name):      #4 #declaring function with parameter
         return ("Hello there " + func(name) + "!")  #5 #8 #retuning string with function having 'name' parameter
@@ -15,3 +14,5 @@ def get_msg(name):    #6      #declaring a 'get_msg' function with parameter 'na
 print(get_msg("john")) #1
 
 ##output: Hello there JOHN! #9
+
+
